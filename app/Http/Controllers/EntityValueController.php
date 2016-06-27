063dccbd508bfd2c1505d3dd60eb57dfb3d356f7
Rafael <rafaelpviegas@gmail.com>
2016-06-27 04:15:13 -0300

    Upload de arquivos

--- app/Http/Controllers/EntityValueController.php
+++ app/Http/Controllers/EntityValueController.php
@@ -32,28 +32,39 @@ class EntityValueController extends Controller
   
     public function set(Request $request, $entity_key, $entity_id)
     {
-  
-        foreach ($request->input('attributes') as $key => $value) {
-         
-            $fields['entity_key'] = $entity_key;
-            $fields['entity_id'] = $entity_id;
-            $fields['attribute_id'] = $key;
-            $fields['value'] = $value;
 
-            $update = Value::where('entity_key', $entity_key)
-                        ->where('entity_id', $entity_id)
-                        ->where('attribute_id', $key)
-                        ->update($fields);
-            
-            if (empty($update)) {
-                Value::create($fields);
+        $files = [];
+        if(!empty($request->all())) {
+            foreach ($request->all() as $key => $value) {
+             
+                if ($request->hasFile($key)) {
+                    $files[] = $key;
+                } else {
+                
+                    $fields['entity_key'] = $entity_key;
+                    $fields['entity_id'] = $entity_id;
+                    $fields['attribute_id'] = $key;
+                    $fields['value'] = $value;
+        
+                    $update = Value::where('entity_key', $entity_key)
+                                ->where('entity_id', $entity_id)
+                                ->where('attribute_id', $key)
+                                ->update($fields);
+                    
+                    if (empty($update)) {
+                        Value::create($fields);
+                    }
+                }
             }
-
         }
-
-        if ($request->hasFile('file')) {
-            $file = $request->file('file');
-            Storage::put($file->getClientOriginalName(), file_get_contents($file));
+  
+        if(!empty($files)) {
+            foreach ($files as $file_attribute) {
+                if ($request->hasFile($file_attribute)) {
+                    $file = $request->file($file_attribute);
+                    Storage::put($file->getClientOriginalName(), file_get_contents($file));
+                }
+            }
         }
 
         return response()->json('created');
